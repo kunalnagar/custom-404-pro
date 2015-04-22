32af0bcc2149b16530b2e341a899beb159d7d4dc
Kunal Nagar <knlnagar@gmail.com>
2015-04-22 21:39:04 +0530

    fixed main plugin file

--- custom-404-pro.php
+++ custom-404-pro.php
@@ -1,29 +1,15 @@
 <?php
 
-/**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              http://kunalnagar.in
- * @since             1.0.0
- * @package           Custom_404_Pro
- *
- * @wordpress-plugin
- * Plugin Name: Custom 404 Pro
- * Plugin URI: https://github.com/kunalnagar/custom-404-pro
- * Description: Override the default 404 page with any page from the Admin Panel.
- * Version: 1.0.0
- * Author: Kunal Nagar
- * Author URI: http://kunalnagar.in
- * License: GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain: custom-404-pro
- * Domain Path: /languages
- */
+/*
+Plugin Name: Custom 404 Pro
+Plugin URI: https://github.com/kunalnagar/custom-404-pro
+Description: Override the default 404 page with any page from the Admin Panel.
+Version: 1.0.0
+Author: Kunal Nagar
+Author URI: http://kunalnagar.in
+License: GPL-2.0+
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+*/
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
